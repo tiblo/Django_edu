36639ff688953f597f6efc81f4df203d1f16f4d6
tiblo <34559256+tiblo@users.noreply.github.com>
2024-03-28 17:02:54 +0900

    Update form.py

--- myproject/myapp/form.py
+++ myproject/myapp/form.py
@@ -16,15 +16,3 @@ class DataForm(ModelForm):
                 'placeholder': '숫자',
             }),
         }
-
-class UserForm(UserCreationForm):
-    # username, password 1, password 2는 기본값으로 처리됨.
-    # 추가적으로 email, first_name을 받기 위해, 수동으로 잡아줘야함.
-    # username은 id를 의미하며, 사용자 이름은 first_name과 last_name으로 되어 있음.
-    username = CharField(label='사용자 ID')
-    email = EmailField(label="이메일")
-    first_name = CharField(label='사용자 이름')
-
-    class Meta:
-        model = User
-        fields = ('username', 'password1', 'password2', 'email', 'first_name')
