864bd41120f6449c7d38791f0b42b655c91700ec
tiblo <34559256+tiblo@users.noreply.github.com>
2024-03-28 17:03:12 +0900

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -9,8 +9,4 @@ urlpatterns = [
     path('write', views.write, name='write'),
     path('update/<int:id>', views.update, name='update'),
     path('delete/<int:id>', views.delete, name='delete'),
-    path('join', views.join, name='join'),
-    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    # path('logout', auth_views.LogoutView.as_view(), name='logout'),
-    path('logout', views.logout_view, name='logout'),
 ]
